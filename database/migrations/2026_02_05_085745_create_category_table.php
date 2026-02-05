da3ff69d0bc0c39a9840b167a0d947f6f79d1a3f
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-02-05 10:35:30 +0100

    Jour_3_Ex_1_Première_migration

---
+++ database/migrations/2026_02_05_085745_create_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();                    // Clé primaire auto-incrémentée
+            $table->string('name');          // VARCHAR(255)
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();     // TEXT
+            $table->timestamps();            // created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category');
+    }
+};
